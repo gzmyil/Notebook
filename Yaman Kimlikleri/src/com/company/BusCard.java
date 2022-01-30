8e6748223991e5508276c5703ace199354499d6f
gzmyil <96871298+gzmyil@users.noreply.github.com>
2022-01-31 01:03:46 +0300

    Delete Yaman Kimlikleri directory

--- Yaman Kimlikleri/src/com/company/BusCard.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class BusCard extends SchoolCard{
-    public void getCardTipi(String öğrenci){
-        System.out.println("öğrenci");
-    }
-}
--- Yaman Kimlikleri/src/com/company/SchoolCard.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class SchoolCard extends Identitiy{
-    public void getokul(String cumhuriyet_lisesi){
-    }
-    public void getsinif(){
-
-    }
-}
